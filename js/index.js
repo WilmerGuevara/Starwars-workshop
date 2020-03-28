89ca214fb3440111fac8006abfc285b445dae77a
eechava6 <eechava6@eafit.edu.co>
2020-03-28 00:56:11 -0500

    Update: working version without resolving promises

--- js/index.js
+++ js/index.js
@@ -4,11 +4,12 @@
 //Reorders the data
 function newOrder(data){
     console.log(data);
-    let movies = data.results
-    let planetKeys = ['name','terrain','gravity','population']
-    let charactersKeys = ['name','gender','hair_color','skin_color','eye_color','height',['homeworld','name'],['species','name','language','average_height']]
-    let starshipKeys = ['name','model','manufacturer','passengers']
-    let newOrder = []
+    let movies = data.results;
+    let planetKeys = ['name','terrain','gravity','population'];
+    let charactersKeys = ['name','gender','hair_color','skin_color','eye_color','height',['homeworld','name'],['species','name','language','average_height']];
+    let starshipKeys = ['name','model','manufacturer','passengers'];
+    let newOrder = [];
+
     movies.forEach(movie => newOrder.push({
         'name':movie.title,
         'planets': parseElements(movie.planets,planetKeys),
@@ -16,40 +17,34 @@ function newOrder(data){
         'starships': parseElements(movie.starships,starshipKeys)
     }));
 
-    console.log(newOrder)
+    return newOrder
 }
 
-//Filters every JSON
-function parseElements(elements,keys){
-    let filtered = []
-    if(Array.isArray(elements)){
-        elements.forEach(element => filtered.push(parseElement(element,keys)))
-    }else{
-        filtered = parseElement(elements)
+//Check if is array or not, and executes callback
+function checkArray(elements,callback){
+    if(!Array.isArray(elements)){
+        return new Promise((resolve) => {
+            resolve(callback(elements));
+        });
     }
-    return filtered;
+    let arr = [];
+    elements.forEach(element => arr.push(callback(element)));
+    return new Promise((resolve) => {
+        resolve(arr);
+    });
 }
 
 //Filters elements based in key and elements
-function parseElement(element,keys){
-    httpGetAsync(element, res => {
-        let value = JSON.parse(res);
-        let elementFiltered = {};
-        if(Array.isArray(keys)){
-            keys.forEach(key => {
-                if(Array.isArray(key)){ 
-                    elementFiltered[key[0]] = parseElements(value[key[0]],key.slice(1));
-                }else{
-                    elementFiltered[key] = value[key];
-                }  
-            })
-        }
-        else{
-            console.log('here')
-            elementFiltered[keys] = value[keys];
-        }
-        return elementFiltered;
+async function parseElements(elements,keys){
+    let response = await checkArray(elements,httpGetAsync);
+    let result = await Promise.all(response)
+    let firstFilter = await checkArray(result, async (object) => {
+        let filtered = {};
+        keys.forEach(key => filtered[key] = object[key]);
+        return filtered;
     });
+    let final = await Promise.all(firstFilter)
+    return Promise.resolve(final);
 }
 
 //HTTP request
@@ -59,7 +54,7 @@ var httpGetAsync = (url) => {
         xmlHttp.onreadystatechange =  _=> { 
             if (xmlHttp.readyState !== 4) return;
             if (xmlHttp.status >= 200 && xmlHttp.status < 300) {
-                resolve(xmlHttp.responseText);
+                resolve(JSON.parse(xmlHttp.responseText));
             }else{
                 reject({
                     status: xmlHttp.status,
@@ -76,7 +71,7 @@ var httpGetAsync = (url) => {
 //Function to run the script
 async function startAll(){
     let data = await httpGetAsync('https://swapi.co/api/films');
-    newOrder(JSON.parse(data))
+    console.log(newOrder(data))
 }
 
 startAll()
